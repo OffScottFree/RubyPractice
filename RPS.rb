f35960010743aa2250bde34c16892caaf853ad58
offscottfree <122756810+OffScottFree@users.noreply.github.com>
2023-01-27 09:53:59 -0700

    initial commit

--- RPS.rb
+++ RPS.rb
@@ -1,44 +1,65 @@
 #! usr/bin/ ruby
 
-puts 'Rock (R), Paper (P), Scissors! (S)'
+puts "Best of five!"
 
-ropasc = gets.chomp
+play_again = "Y"
+compscore = 0
+score = 0
+while play_again.upcase() == "Y" 
 
-case ropasc
-when 'r' , 'R'
-  item = 'Rock' 
-when 'p' , 'P'
-  item = 'Paper'
-when 's' , 'S'
-  item = 'Scissors'
-when 'd' , 'D'
-  item = 'Dynamite'
-else 
-  (sleep 1) && abort("No cheating!")
+  puts 'Rock (R), Paper (P), Scissors! (S)'
+
+  ropasc = gets.chomp
+
+  case ropasc.upcase()
+  when  'R'
+    item = 'Rock' 
+  when 'P'
+    item = 'Paper'
+  when 'S'
+    item = 'Scissors'
+  when 'D'
+    item = 'Dynamite'
+  else 
+    (sleep 0.75) && abort("No cheating!")
+  end
+
+  puts "You chose #{item}"
+
+  citem = ['Rock', 'Paper', 'Scissors'].sample
+
+  sleep 0.75
+
+  puts "Computer chose #{citem}"
+
+  sleep 0.75
+
+  if item == citem 
+    puts 'Darn, a draw' 
+  elsif item == "Rock" && citem == "Scissors"
+    puts 'Winner! Suck it computer!'; score += 1
+  elsif item == "Scissors" && citem == "Paper"
+    puts 'Winner! Suck it computer!'; score += 1
+  elsif item == 'Paper' && citem == 'Rock'
+    puts 'Winner! Suck it computer!'; score += 1
+  elsif item == 'Dynamite'
+    score += 1; puts "You autowin. Hooray for cheating!"
+  else
+    compscore += 1; puts 'You Lose! Sucks to Suck!' 
+  end
+
+  sleep 0.75 
+
+  if score == 5 
+    abort ("You win #{score} - #{compscore}")
+  elsif compscore == 5
+    abort ("You lose #{compscore} - #{score}")
+  else
+    puts "The score is #{score} - #{compscore}"
+  end
+ 
+  puts "Would you like to play again? (Y/N)"
+  play_again = gets.chomp
+
+
 end
-
-puts "You chose #{item}"
-
-citem = ['Rock', 'Paper', 'Scissors'].sample
-
-sleep 0.75
-
-puts "Computer chose #{citem}"
-
-sleep 0.75
-
-if item == citem 
-  puts 'Darn, a draw'
-elsif item == "Rock" && citem == "Scissors"
-  puts 'Winner! Suck it computer!'
-elsif item == "Scissors" && citem == "Paper"
-  puts 'Winner! Suck it computer!'
-elsif item == 'Paper' && citem == 'Rock'
-  puts 'Winner! Suck it computer!' 
-elsif item == 'Dynamite'
-  puts 'You autowin. Hooray for cheating!'
-else
- puts 'You Lose! Sucks to Suck!'
-end
-
-#Maybe add a cumulative score? Option to play again.
